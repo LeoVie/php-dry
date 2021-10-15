2732da6e4eb9a4abacadfd910a9d2b147a439169
Leo Viezens <leo.viezens@web.de>
2021-10-15 12:57:21 +0200

    Make constructor public to allow symfony to instantiate the class

--- src/Tokenize/Normalize/VariableNormalizer.php
+++ src/Tokenize/Normalize/VariableNormalizer.php
@@ -11,7 +11,7 @@ class VariableNormalizer implements TokenNormalizer
 {
     private ReplacementRegister $variableReplacementRegister;
 
-    private function __construct()
+    public function __construct()
     {
         $this->variableReplacementRegister = ReplacementRegister::create('$x');
     }
