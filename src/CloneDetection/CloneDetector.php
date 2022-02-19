eae1424bd85b78f93e2417b2858601c9c5445849
Leo Viezens <leo.viezens@web.de>
2022-02-19 08:43:51 +0100

    Refactor

--- src/CloneDetection/CloneDetector.php
+++ src/CloneDetection/CloneDetector.php
@@ -16,12 +16,27 @@ class CloneDetector
      */
     public function detect(array $sourceCloneCandidates, string $type): array
     {
+        return $this->createSourceClonesFromSourceCloneCandidates($type, $sourceCloneCandidates);
+    }
+
+    /**
+     * @param SourceCloneCandidate[] $sourceCloneCandidates
+     *
+     * @return SourceClone[]
+     */
+    private function findSourceCloneCandidatesWithMultipleMethods(array $sourceCloneCandidates): array
+    {
+        return array_filter(
+            $sourceCloneCandidates,
+            fn(SourceCloneCandidate $sc): bool => $sc->getMethodsCollection()->count() > 1
+        );
+    }
+
+    private function createSourceClonesFromSourceCloneCandidates(string $type, array $sourceCloneCandidates): array
+    {
         return array_map(
             fn(SourceCloneCandidate $scc): SourceClone => SourceClone::create($type, $scc->getMethodsCollection()),
-            array_filter(
-                $sourceCloneCandidates,
-                fn(SourceCloneCandidate $sc): bool => $sc->getMethodsCollection()->count() > 1
-            )
+            $this->findSourceCloneCandidatesWithMultipleMethods($sourceCloneCandidates)
         );
     }
 }
