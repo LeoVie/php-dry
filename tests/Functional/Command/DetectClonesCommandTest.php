717c4d75841132685282b376dc024fddfea0e9f0
Leo Viezens <leo.viezens@web.de>
2022-02-21 14:34:48 +0100

    Make test more flexible

--- tests/Functional/Command/DetectClonesCommandTest.php
+++ tests/Functional/Command/DetectClonesCommandTest.php
@@ -43,56 +43,56 @@ class DetectClonesCommandTest extends KernelTestCase
         $type1Clones = "TYPE_1
 ------
 
- * /home/ubuntu/development/php-dry/tests/Functional/Command/../../testdata/clone-detection-testdata/01_A.php: foo (5 (position 50) - 13 (position 231) (8 lines))
- * /home/ubuntu/development/php-dry/tests/Functional/Command/../../testdata/clone-detection-testdata/03_A_Exact_Copy.php: foo (5 (position 61) - 13 (position 242) (8 lines))
- * /home/ubuntu/development/php-dry/tests/Functional/Command/../../testdata/clone-detection-testdata/04_A_Additional_Whitespaces.php: foo (5 (position 73) - 14 (position 276) (9 lines))
- * /home/ubuntu/development/php-dry/tests/Functional/Command/../../testdata/clone-detection-testdata/05_A_Additional_Comments.php: foo (5 (position 70) - 20 (position 440) (15 lines))
- * /home/ubuntu/development/php-dry/tests/Functional/Command/../../testdata/clone-detection-testdata/06_A_Changed_Layout.php: foo (5 (position 65) - 9 (position 210) (4 lines))";
+ * ${testdataDir}01_A.php: foo (5 (position 50) - 13 (position 231) (8 lines))
+ * ${testdataDir}03_A_Exact_Copy.php: foo (5 (position 61) - 13 (position 242) (8 lines))
+ * ${testdataDir}04_A_Additional_Whitespaces.php: foo (5 (position 73) - 14 (position 276) (9 lines))
+ * ${testdataDir}05_A_Additional_Comments.php: foo (5 (position 70) - 20 (position 440) (15 lines))
+ * ${testdataDir}06_A_Changed_Layout.php: foo (5 (position 65) - 9 (position 210) (4 lines))";
         self::assertStringContainsString(str_replace(["\n", "\r"], '', $type1Clones), str_replace(["\n", "\r"], '', $output));
 
         $type2Clones = "TYPE_2
 ------
 
- * /home/ubuntu/development/php-dry/tests/Functional/Command/../../testdata/clone-detection-testdata/01_A.php: foo (5 (position 50) - 13 (position 231) (8 lines))
- * /home/ubuntu/development/php-dry/tests/Functional/Command/../../testdata/clone-detection-testdata/03_A_Exact_Copy.php: foo (5 (position 61) - 13 (position 242) (8 lines))
- * /home/ubuntu/development/php-dry/tests/Functional/Command/../../testdata/clone-detection-testdata/04_A_Additional_Whitespaces.php: foo (5 (position 73) - 14 (position 276) (9 lines))
- * /home/ubuntu/development/php-dry/tests/Functional/Command/../../testdata/clone-detection-testdata/05_A_Additional_Comments.php: foo (5 (position 70) - 20 (position 440) (15 lines))
- * /home/ubuntu/development/php-dry/tests/Functional/Command/../../testdata/clone-detection-testdata/06_A_Changed_Layout.php: foo (5 (position 65) - 9 (position 210) (4 lines))
- * /home/ubuntu/development/php-dry/tests/Functional/Command/../../testdata/clone-detection-testdata/07_A_Changed_Variable_Names.php: foo (5 (position 73) - 13 (position 268) (8 lines))
- * /home/ubuntu/development/php-dry/tests/Functional/Command/../../testdata/clone-detection-testdata/08_A_Changed_Method_Names.php: bar (5 (position 71) - 13 (position 252) (8 lines))
- * /home/ubuntu/development/php-dry/tests/Functional/Command/../../testdata/clone-detection-testdata/09_A_Changed_Literals.php: foo (5 (position 67) - 13 (position 248) (8 lines))";
+ * ${testdataDir}01_A.php: foo (5 (position 50) - 13 (position 231) (8 lines))
+ * ${testdataDir}03_A_Exact_Copy.php: foo (5 (position 61) - 13 (position 242) (8 lines))
+ * ${testdataDir}04_A_Additional_Whitespaces.php: foo (5 (position 73) - 14 (position 276) (9 lines))
+ * ${testdataDir}05_A_Additional_Comments.php: foo (5 (position 70) - 20 (position 440) (15 lines))
+ * ${testdataDir}06_A_Changed_Layout.php: foo (5 (position 65) - 9 (position 210) (4 lines))
+ * ${testdataDir}07_A_Changed_Variable_Names.php: foo (5 (position 73) - 13 (position 268) (8 lines))
+ * ${testdataDir}08_A_Changed_Method_Names.php: bar (5 (position 71) - 13 (position 252) (8 lines))
+ * ${testdataDir}09_A_Changed_Literals.php: foo (5 (position 67) - 13 (position 248) (8 lines))";
         self::assertStringContainsString(str_replace(["\n", "\r"], '', $type2Clones), str_replace(["\n", "\r"], '', $output));
 
         $type3Clones = "TYPE_3
 ------
 
- * /home/ubuntu/development/php-dry/tests/Functional/Command/../../testdata/clone-detection-testdata/01_A.php: foo (5 (position 50) - 13 (position 231) (8 lines))
- * /home/ubuntu/development/php-dry/tests/Functional/Command/../../testdata/clone-detection-testdata/03_A_Exact_Copy.php: foo (5 (position 61) - 13 (position 242) (8 lines))
- * /home/ubuntu/development/php-dry/tests/Functional/Command/../../testdata/clone-detection-testdata/04_A_Additional_Whitespaces.php: foo (5 (position 73) - 14 (position 276) (9 lines))
- * /home/ubuntu/development/php-dry/tests/Functional/Command/../../testdata/clone-detection-testdata/05_A_Additional_Comments.php: foo (5 (position 70) - 20 (position 440) (15 lines))
- * /home/ubuntu/development/php-dry/tests/Functional/Command/../../testdata/clone-detection-testdata/06_A_Changed_Layout.php: foo (5 (position 65) - 9 (position 210) (4 lines))
- * /home/ubuntu/development/php-dry/tests/Functional/Command/../../testdata/clone-detection-testdata/07_A_Changed_Variable_Names.php: foo (5 (position 73) - 13 (position 268) (8 lines))
- * /home/ubuntu/development/php-dry/tests/Functional/Command/../../testdata/clone-detection-testdata/08_A_Changed_Method_Names.php: bar (5 (position 71) - 13 (position 252) (8 lines))
- * /home/ubuntu/development/php-dry/tests/Functional/Command/../../testdata/clone-detection-testdata/09_A_Changed_Literals.php: foo (5 (position 67) - 13 (position 248) (8 lines))
- * /home/ubuntu/development/php-dry/tests/Functional/Command/../../testdata/clone-detection-testdata/11_A_Removed_Statements.php: foo (5 (position 69) - 12 (position 231) (7 lines))
- * /home/ubuntu/development/php-dry/tests/Functional/Command/../../testdata/clone-detection-testdata/12_A_Changed_Statement_Order.php: foo (5 (position 74) - 13 (position 255) (8 lines))";
+ * ${testdataDir}01_A.php: foo (5 (position 50) - 13 (position 231) (8 lines))
+ * ${testdataDir}03_A_Exact_Copy.php: foo (5 (position 61) - 13 (position 242) (8 lines))
+ * ${testdataDir}04_A_Additional_Whitespaces.php: foo (5 (position 73) - 14 (position 276) (9 lines))
+ * ${testdataDir}05_A_Additional_Comments.php: foo (5 (position 70) - 20 (position 440) (15 lines))
+ * ${testdataDir}06_A_Changed_Layout.php: foo (5 (position 65) - 9 (position 210) (4 lines))
+ * ${testdataDir}07_A_Changed_Variable_Names.php: foo (5 (position 73) - 13 (position 268) (8 lines))
+ * ${testdataDir}08_A_Changed_Method_Names.php: bar (5 (position 71) - 13 (position 252) (8 lines))
+ * ${testdataDir}09_A_Changed_Literals.php: foo (5 (position 67) - 13 (position 248) (8 lines))
+ * ${testdataDir}11_A_Removed_Statements.php: foo (5 (position 69) - 12 (position 231) (7 lines))
+ * ${testdataDir}12_A_Changed_Statement_Order.php: foo (5 (position 74) - 13 (position 255) (8 lines))";
         self::assertStringContainsString(str_replace(["\n", "\r"], '', $type3Clones), str_replace(["\n", "\r"], '', $output));
 
         $type4Clones = "TYPE_4
 ------
 
- * /home/ubuntu/development/php-dry/tests/Functional/Command/../../testdata/clone-detection-testdata/01_A.php: foo (5 (position 50) - 13 (position 231) (8 lines))
- * /home/ubuntu/development/php-dry/tests/Functional/Command/../../testdata/clone-detection-testdata/03_A_Exact_Copy.php: foo (5 (position 61) - 13 (position 242) (8 lines))
- * /home/ubuntu/development/php-dry/tests/Functional/Command/../../testdata/clone-detection-testdata/04_A_Additional_Whitespaces.php: foo (5 (position 73) - 14 (position 276) (9 lines))
- * /home/ubuntu/development/php-dry/tests/Functional/Command/../../testdata/clone-detection-testdata/05_A_Additional_Comments.php: foo (5 (position 70) - 20 (position 440) (15 lines))
- * /home/ubuntu/development/php-dry/tests/Functional/Command/../../testdata/clone-detection-testdata/06_A_Changed_Layout.php: foo (5 (position 65) - 9 (position 210) (4 lines))
- * /home/ubuntu/development/php-dry/tests/Functional/Command/../../testdata/clone-detection-testdata/07_A_Changed_Variable_Names.php: foo (5 (position 73) - 13 (position 268) (8 lines))
- * /home/ubuntu/development/php-dry/tests/Functional/Command/../../testdata/clone-detection-testdata/08_A_Changed_Method_Names.php: bar (5 (position 71) - 13 (position 252) (8 lines))
- * /home/ubuntu/development/php-dry/tests/Functional/Command/../../testdata/clone-detection-testdata/09_A_Changed_Literals.php: foo (5 (position 67) - 13 (position 248) (8 lines))
- * /home/ubuntu/development/php-dry/tests/Functional/Command/../../testdata/clone-detection-testdata/10_A_Additional_Statements.php: foo (5 (position 72) - 14 (position 290) (9 lines))
- * /home/ubuntu/development/php-dry/tests/Functional/Command/../../testdata/clone-detection-testdata/11_A_Removed_Statements.php: foo (5 (position 69) - 12 (position 231) (7 lines))
- * /home/ubuntu/development/php-dry/tests/Functional/Command/../../testdata/clone-detection-testdata/12_A_Changed_Statement_Order.php: foo (5 (position 74) - 13 (position 255) (8 lines))
- * /home/ubuntu/development/php-dry/tests/Functional/Command/../../testdata/clone-detection-testdata/13_A_Changed_Syntax.php: foo (5 (position 65) - 11 (position 210) (6 lines))";
+ * ${testdataDir}01_A.php: foo (5 (position 50) - 13 (position 231) (8 lines))
+ * ${testdataDir}03_A_Exact_Copy.php: foo (5 (position 61) - 13 (position 242) (8 lines))
+ * ${testdataDir}04_A_Additional_Whitespaces.php: foo (5 (position 73) - 14 (position 276) (9 lines))
+ * ${testdataDir}05_A_Additional_Comments.php: foo (5 (position 70) - 20 (position 440) (15 lines))
+ * ${testdataDir}06_A_Changed_Layout.php: foo (5 (position 65) - 9 (position 210) (4 lines))
+ * ${testdataDir}07_A_Changed_Variable_Names.php: foo (5 (position 73) - 13 (position 268) (8 lines))
+ * ${testdataDir}08_A_Changed_Method_Names.php: bar (5 (position 71) - 13 (position 252) (8 lines))
+ * ${testdataDir}09_A_Changed_Literals.php: foo (5 (position 67) - 13 (position 248) (8 lines))
+ * ${testdataDir}10_A_Additional_Statements.php: foo (5 (position 72) - 14 (position 290) (9 lines))
+ * ${testdataDir}11_A_Removed_Statements.php: foo (5 (position 69) - 12 (position 231) (7 lines))
+ * ${testdataDir}12_A_Changed_Statement_Order.php: foo (5 (position 74) - 13 (position 255) (8 lines))
+ * ${testdataDir}13_A_Changed_Syntax.php: foo (5 (position 65) - 11 (position 210) (6 lines))";
 
         self::assertStringContainsString(str_replace(["\n", "\r"], '', $type4Clones), str_replace(["\n", "\r"], '', $output));
     }
